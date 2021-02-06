61e59b52bfcdf96a79956e42b956f3872f4f0d14
Maciej Wódke <maciej@mwpro.pl>
2021-02-06 12:41:11 +0100

    Delete product and started work on pagination

---
+++ sick-fits/frontend/components/DeleteProduct.js
@@ -0,0 +1,36 @@
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+
+const DELETE_PRODUCT_MUTATION = gql`
+  mutation DELETE_PRODUCT_MUTATION($id: ID!) {
+    deleteProduct(id: $id) {
+      id
+      name
+    }
+  }
+`;
+
+function update(cache, payload) {
+  cache.evict(cache.identify(payload.data.deleteProduct));
+}
+
+export default function DeleteProduct({ id, children }) {
+  const [deleteProduct, { loading }] = useMutation(DELETE_PRODUCT_MUTATION, {
+    variables: { id },
+    update,
+  });
+  return (
+    <button
+      type="button"
+      disabled={loading}
+      onClick={() => {
+        // eslint-disable-next-line no-restricted-globals
+        if (confirm('Are you sure you want to delete this item?')) {
+          deleteProduct().catch((err) => alert(err.message));
+        }
+      }}
+    >
+      {children}
+    </button>
+  );
+}
